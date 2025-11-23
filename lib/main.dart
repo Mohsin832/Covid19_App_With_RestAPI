08e498297a19495626aabb96a06a249d37db00af
Mohsin832 <mm8116900@gmail.com>
2025-11-23 13:25:18 +0500

    splash screen

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:covidapp/view/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue),
+      home: Splashscreen(),
+    );
+  }
+}
---
+++ lib/view/splashscreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen>
+    with TickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: .center,
+          crossAxisAlignment: .center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
