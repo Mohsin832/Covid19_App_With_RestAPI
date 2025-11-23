56819b04ef5770de9561dabfd5f0410d55fcb6d0
Mohsin832 <mm8116900@gmail.com>
2025-11-23 14:26:35 +0500

    splash screen

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
@@ -9,11 +10,14 @@ class Splashscreen extends StatefulWidget {
 
 class _SplashscreenState extends State<Splashscreen>
     with TickerProviderStateMixin {
+    late final AnimationController _controller = AnimationController(duration: const Duration(seconds: 3), vsync: this)..repeat();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +26,20 @@ class _SplashscreenState extends State<Splashscreen>
         child: Column(
           mainAxisAlignment: .center,
           crossAxisAlignment: .center,
-          children: [],
+          children: [
+            AnimatedBuilder(animation: _controller ,
+            child: Container(
+              height: 200,
+              width: 200,
+              child: const Center(
+                child: Image(image: AssetImage("assets/images/virus.png")),
+              ),
+            ),
+             builder: (BuildContext context, Widget? child){
+              return Transform.rotate(angle: _controller.value * 2.0 * math.pi, 
+              child: child,);
+            })
+          ],
         ),
       ),
     );
